71c6eda9f81d9d26993ace0bbe11e5760446e63a
Nguyễn Hoài Thanh <thanh14147@gmail.com>
2024-01-12 21:22:42 +0700

    Feat: add ResetPasswordTokenExpiresAt for Account

---
+++ Entities/Migrations/20240112142055_Add_ResetPasswordTokenExpiresAt_AccountTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ResetPasswordTokenExpiresAt_AccountTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetPasswordTokenExpiresAt",
+                table: "Accounts",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetPasswordTokenExpiresAt",
+                table: "Accounts");
+        }
+    }
+}
